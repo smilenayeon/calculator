6de15aeda5060be9fac7698eb1bbf70f267148c8
Na Yeon <smilenayeon@naver.com>
2023-12-08 00:47:11 +0900

    grab user input data and sanitize the data using filter_input and htmlspecialchars

--- index.php
+++ index.php
@@ -8,9 +8,27 @@
 </head>
 <body>
 
-<form action="index.php" method="get">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+<input type="number" name="num01" placeholder="first number"/>
+<select  name="operator">
+    <option value="add">+</option>
+    <option value="subtract">-</option>
+    <option value="multiply">*</option>
+    <option value="divide">/</option>
+</select>
+<input type="number" name="number02" placeholder="second number"/>
+<button>Calculate</button>
 
 </form>
+
+<?php
+if( $_SERVER["REQUEST_METHOD"] == "POST"){
+$num01 = filter_input(INPUT_POST,"num01", FILTER_SANITIZE_NUMBER_FLOAT);
+$num02 = filter_input(INPUT_POST,"num02", FILTER_SANITIZE_NUMBER_FLOAT);
+$operator = htmlspecialchars($_POST["operator"]);
+
+}
+?>
 
 </body>
 </html>
