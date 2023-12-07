204311143e19e52d62c41221e6b9ae7318b412e2
Na Yeon <smilenayeon@naver.com>
2023-12-08 00:27:53 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diana's calculator</title>
+    <link rel="stylesheet" href="/CSS/index.css">
+</head>
+<body>
+
+<form action="index.php" method="get">
+
+</form>
+
+</body>
+</html>
